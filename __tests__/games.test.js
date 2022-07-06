7bf606400cb835dd8643f178cd78963ab05e94e2
adamjey <aljeylaani@gmail.com>
2022-07-06 11:44:14 +0100

    done ticket 4

--- __tests__/games.test.js
+++ __tests__/games.test.js
@@ -1,7 +1,13 @@
 const db = require("../db/connection");
 const app = require("../app");
 const request = require("supertest");
+const seed = require("../db/seeds/seed");
+const testData = require("../db/data/test-data");
 
+
+beforeEach(()=> {
+    return seed(testData)
+});
 
 afterAll(() => {
   return db.end();
@@ -32,7 +38,32 @@ describe("GET /api/categories", () => {
           .then(({ body }) => {
             expect(body.msg).toBe("Not Found");
           });
+        
       });
+});
+    
+
+
+describe("GET /api/reviews/:review_id", () => {
+    test('200: responds with a review object with the passed id', () => {
+        return request(app)
+            .get("/api/reviews/1")
+            .expect(200)
+            .then(({ body }) => {
+                const { review } = body;
+                expect(review).toMatchObject({
+                    review_id: 1,
+                    title: 'Agricola',
+                    review_body: 'Farmyard fun!',
+                    designer: 'Uwe Rosenberg',
+                    review_img_url: 'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+                    votes: 1,
+                    category: 'euro game',
+                    owner: 'mallionaire',
+                    created_at:expect.any(String),
+                })
+            })
     });
+})
 
     
--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express");
-const {getCategories } = require ("./controller/catg-controller")
+const { getCategories } = require ("./controller/catg-controller")
+const { getReview } = require ("./controller/review-contreller")
 const app = express();
+
+
+
 
 //ROUTER
 app.get("/api/categories", getCategories);
+app.get("/api/reviews/:review_id", getReview);
+
 
 // custom errors
-
 app.use("/*", (req, res, next) => {
     res.status(404).send({ msg: "Not Found" });
   });
+
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ msg: "Not Found" });
+
+  console.log(err);
+});
 
 // PSQL errors
 
